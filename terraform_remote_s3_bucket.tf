231c1efa21ce5bab70ea8284345b3977a38d0a86
tiddnet <todd.c.campbell@gmail.com>
2018-01-01 15:39:43 -0500

    updated bucket names

--- terraform_remote_s3_bucket.tf
+++ terraform_remote_s3_bucket.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # terraform state file setup
 # create an S3 bucket to store the state file in (can this be abstracted? Revisit when we do other s3 bucket creations)
 resource "aws_s3_bucket" "terraform-state-storage-s3" {
-    bucket = "scientiamobile-terraform-remote-state-storage-s3"
+    bucket = "xxxxxxxxxxxxxxxx-terraform-remote-state-storage-s3"
  
     versioning {
       enabled = true
