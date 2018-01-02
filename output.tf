561a936e660f1025cb924069a1705ce5e138bff4
tiddnet <todd.c.campbell@gmail.com>
2018-01-02 01:33:44 -0500

    exposed zone.

--- output.tf
+++ output.tf
@@ -2,6 +2,14 @@ output "aws_route53_delegation_set.main.id" {
   value = "${aws_route53_delegation_set.main.id}"
 }
 
+output "aws_route53_zone.main.id" {
+  value = "${aws_route53_zone.main.id}"
+}
+
+output "aws_route53_zone.main.name" {
+  value = "${aws_route53_zone.main.name}"
+}
+
 #output "my-source-ami" {
 #  value = "${data.aws_ami.deploy.id}"
 #}
