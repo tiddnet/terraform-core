662344f055014d516ac0ea1174f1ae3844907316
Todd C. Campbell <todd.c.campbell@gmail.com>
2018-01-02 18:25:01 -0500

    Added public subnet module. Needs more outputs.

--- terraform_remote_dynamo_lock.tf
+++ terraform_remote_dynamo_lock.tf
@@ -1,7 +1,7 @@
 # Let's not touch this for awhile. We don't use dynamo elsewhere, so let's not muck with the remote state file storage unless we really need to.
 # create a dynamodb table for locking the state file
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name           = "terraform-state-lock-dynamo"
+  name           = "terraform-state-lock-dynamo.2018"
   hash_key       = "LockID"
   read_capacity  = 20
   write_capacity = 20
