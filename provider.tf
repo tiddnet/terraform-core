ed313cf6365bb1b2e66f92ac45df386c4ca005f2
Todd C. Campbell <todd.c.campbell@gmail.com>
2018-01-24 22:53:22 -0500

    Added a provider version contraint.

--- provider.tf
+++ provider.tf
@@ -1,5 +1,7 @@
 # Configure the AWS Provider
 provider "aws" {
+  # abstract the version constraint into the vars.tf or elsewhere
+  version = "~> 1.0"
   shared_credentials_file = "~/.aws/credentials"
   region                  = "${var.region}"
 }
