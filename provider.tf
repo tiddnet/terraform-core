be329ef3dd8888d42b475ad411185908a6f1f675
Todd C. Campbell <todd.c.campbell@gmail.com>
2018-01-24 22:54:42 -0500

    update version contraint.

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 # Configure the AWS Provider
 provider "aws" {
   # abstract the version constraint into the vars.tf or elsewhere
-  version = "~> 1.0"
+  version = "~> 1.7"
   shared_credentials_file = "~/.aws/credentials"
   region                  = "${var.region}"
 }
