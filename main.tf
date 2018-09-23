393b6c7ee7652be0d45cec444b787d6586d0c6bb
Todd C. Campbell <tiddnet@users.noreply.github.com>
2018-09-23 12:40:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,28 +43,4 @@ resource "aws_route53_record" "ns" {
 # Copy AMIs  
 
 
-#module "search" {
-#  source = "search"
-#  region = "us-east-1"
-#}
-#
-#module "copy-to-us-east-2" {
-#  source   = "copy"
-#  region   = "us-east-2"
-#  copy_ami = "${module.search.my-source-ami}"
-#}
-#
-#module "copy-to-us-west-1" {
-#  source   = "copy"
-#  region   = "us-west-1"
-#  copy_ami = "${module.search.my-source-ami}"
-#}
-#
-#module "copy-to-us-west-2" {
-#  source   = "copy"
-#  region   = "us-west-2"
-#  copy_ami = "${module.search.my-source-ami}"
-#}
-#
-#
 
